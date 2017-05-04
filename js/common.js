e2827cf11ad2c6ed6b02d116610d0db3133636fb
Ilya Lunev <i.b.lunev@gmail.com>
2017-05-04 17:03:20 +0500

    popup

--- js/common.js
+++ js/common.js
@@ -28,6 +28,42 @@ $(function() {
 		offset: '80%'
 	});
 
+// Magnific popup
+	$(".popup").magnificPopup();
+
+	// $(".popup").magnificPopup({
+	// 	mainClass: 'mfp-zoom-in',
+	// 	// delegate: 'a',
+	// 	type: 'inline',
+	// 	tLoading: '',
+	// 	gallery:{
+	// 		enabled:true,
+	// 	},
+	// 	removalDelay: 300,
+	// 	callbacks: {
+	// 		beforeChange: function() {
+	// 			this.items[0].src = this.items[0].src + '?=' + Math.random();
+	// 		},
+	// 		open: function() {
+	// 			$.magnificPopup.instance.next = function() {
+	// 				var self = this;
+	// 				self.wrap.removeClass('mfp-image-loaded');
+	// 				setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+	// 			}
+	// 			$.magnificPopup.instance.prev = function() {
+	// 				var self = this;
+	// 				self.wrap.removeClass('mfp-image-loaded');
+	// 				setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+	// 			}
+	// 		},
+	// 		imageLoadComplete: function() {
+	// 			var self = this;
+	// 			setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+	// 		}
+	// 	}
+	// });
+
+
 	//Toggle Mnu Function
 	$(".toggle-mnu").click(function() {
 		$(this).toggleClass("on");
